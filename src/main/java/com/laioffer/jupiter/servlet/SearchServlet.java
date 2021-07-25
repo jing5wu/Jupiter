0995c1854858a906f8ee83a6a8766a31fa16b618
JingjieW <wujingjiejy@gmail.com>
2021-07-25 12:51:09 -0700

    my first commit

---
+++ src/main/java/com/laioffer/jupiter/servlet/SearchServlet.java
@@ -0,0 +1,31 @@
+package com.laioffer.jupiter.servlet;
+
+import com.laioffer.jupiter.external.TwitchClient;
+import com.laioffer.jupiter.external.TwitchException;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "SearchServlet", value = "/search")
+public class SearchServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String gameId = request.getParameter("game_id");
+        if (gameId == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        TwitchClient client = new TwitchClient();
+        try {
+
+            ServletUtil.writeData(
+                    response, client.searchItems(gameId)
+            );
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
+    }
+}
